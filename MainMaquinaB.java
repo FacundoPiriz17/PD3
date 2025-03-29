70c99f2a00021f5f12b06c083908368cc623ffe8
FacundoPiriz17 <facundo.piriz@correo.ucu.edu.uy>
2025-03-29 20:30:27 -0300

    Implementados métodos de gato

--- MainMaquinaB.java
+++ MainMaquinaB.java
@@ -2,9 +2,9 @@ package PD3;
 
 public class MainMaquinaB {
     public static void main(String[] args) {
-        Gato gato = new Gato("Firulais");
+        Gato gato = new Gato("Oso");
         System.out.println(gato.getNombre());
         gato.maullar();
-        System.out.println(gato.respondeA("Firulais"));
+        System.out.println(gato.respondeA("Oso"));
     }
 }
