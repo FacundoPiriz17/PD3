6fc1b5e841414ffea40497b11e7961bbeceb25f0
FacundoPiriz17 <facundo.piriz@correo.ucu.edu.uy>
2025-03-29 20:23:38 -0300

    Implementada la interfaz mamífero en gato

--- Gato.java
+++ Gato.java
@@ -1,12 +1,13 @@
 package PD3;
 
-public class Gato {
+public class Gato implements Mamifero {
     private String nombre;
 
     public Gato(String nombre) {
         this.nombre = nombre;
     }
 
+    @Override
     public String getNombre() {
         return this.nombre;
     }
@@ -15,7 +16,23 @@ public class Gato {
         System.out.println("Miau");
     }
 
+    @Override
     public boolean respondeA(String unNombre){
         return false; // NUNCA RESPONDEN !!!
+    }
+
+    @Override
+    public void saltar() {
+        System.out.println("El gato está saltando.");
+    }
+
+    @Override
+    public void caminar() {
+        System.out.println("El gato está caminando.");
+    }
+
+    @Override
+    public void correr() {
+        System.out.println("El gato está corriendo.");
     }
 }
